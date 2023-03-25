a183c2c61b271cda9ce95a46df41d2eac8eae4a6
GeoDaz <gfrydz2@gmail.com>
2023-03-25 10:59:44 +0100

    Add image spinner

--- src/components/LinePoint.tsx
+++ src/components/LinePoint.tsx
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from 'react';
 import Link from 'next/link';
 import Image from 'next/image';
 import { makeClassName } from '@/functions';
+import { Spinner } from 'react-bootstrap';
 
 interface Props extends React.ImgHTMLAttributes<any> {
 	name: string;
@@ -29,27 +30,37 @@ const LinePoint: React.FC<Props> = ({ name, style, children, ...props }) => {
 // TODO faire les images d'erreurs en CSS ?
 export const LineImage: React.FC<Props> = ({ name, title, className, style }) => {
 	const [src, setSrc] = useState(`/images/digimon/${name}.jpg`);
+	const [loading, setLoading] = useState(true);
 
 	useEffect(() => {
 		const nextSrc = `/images/digimon/${name}.jpg`;
 		if (src != nextSrc) {
+			setLoading(true);
 			setSrc(nextSrc);
 		}
 	}, [name]);
 
 	return (
-		<Image
-			src={src}
-			onError={(e: React.SyntheticEvent<HTMLImageElement, Event>) => {
-				setSrc('/images/digimon/unknown.jpg');
-			}}
-			alt={name}
-			className={makeClassName('line-img rounded', className)}
-			width={150}
-			height={150}
-			title={title}
-			style={style}
-		/>
+		<>
+			{loading && (
+				<div className="spinner-wrapper">
+					<Spinner animation="border" />
+				</div>
+			)}
+			<Image
+				src={src}
+				onError={(e: React.SyntheticEvent<HTMLImageElement, Event>) => {
+					setSrc('/images/digimon/unknown.jpg');
+				}}
+				onLoad={e => setLoading(false)}
+				alt={name}
+				className={makeClassName('line-img rounded', className)}
+				width={150}
+				height={150}
+				title={title}
+				style={style}
+			/>
+		</>
 	);
 };
 
