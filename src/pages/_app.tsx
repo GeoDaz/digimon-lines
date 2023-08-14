227404af482bfc4d5511ca94af5c332bf1cae901
GeoDaz <gfrydz2@gmail.com>
2023-08-14 10:25:34 +0200

    add analytics

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,12 +2,14 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap-icons/font/bootstrap-icons.css';
 import '@/styles/index.css';
 import type { AppProps } from 'next/app';
+import { Analytics } from '@vercel/analytics/react';
 import ErrorBoundary from '@/components/ErrorBoundary';
 
 export default function App({ Component, pageProps }: AppProps) {
 	return (
 		<ErrorBoundary>
 			<Component {...pageProps} />
+			<Analytics />
 		</ErrorBoundary>
 	);
 }
