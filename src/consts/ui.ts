d6b49907610119b1dff266166ec7f25ba4231eff
GeoDaz <gfrydz2@gmail.com>
2023-08-14 09:24:57 +0200

    missing from commit

--- src/consts/ui.ts
+++ src/consts/ui.ts
@@ -1,3 +1,9 @@
 export const LINE = 'line';
 export const VB = 'vb';
 export const GROUP = 'group';
+
+export const titles: Record<string, string> = {
+	vb: 'Vital Bracelet',
+	line: 'Line',
+	group: 'Group',
+};
