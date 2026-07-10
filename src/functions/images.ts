a83e3dacfd3a474f585b6b03d5e17aa54e7f1089
GeoDaz <gfrydz2@gmail.com>
2026-07-10 09:05:14 +0200

    Set www.coupcritique

--- src/functions/images.ts
+++ src/functions/images.ts
@@ -7,7 +7,7 @@ const imgPathByLicence: Record<string, CallableFunction> = {
 	digimon: (name: string, type: string) =>
 		`/images/${imgDirs.includes(type) ? type : 'digimon'}/${name}.jpg`,
 	pokemon: (name: string) =>
-		`https://coupcritique.fr/images/pokemons/${formatPokemonFileName(name)}.png`,
+		`https://www.coupcritique.fr/images/pokemons/${formatPokemonFileName(name)}.png`,
 };
 
 export default imgPathByLicence;
